deb67300598f8c1ea36f8b546307289e85195b6b
Gaurav Lonkar <zacky.gaurav@gmail.com>
2019-07-11 12:55:28 +0530

    ButterKnife, Passing Data via Explicit Intent, Implicit Intent and Rate App on Google Play

--- app/src/main/java/com/codeavengers/playingwithintents/PaymentsActivity.java
+++ app/src/main/java/com/codeavengers/playingwithintents/PaymentsActivity.java
@@ -1,33 +1,90 @@
 package com.codeavengers.playingwithintents;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PaymentsActivity extends AppCompatActivity
 {
+    // TODO Task: Update the UI of this functionality. Refer uplabs.com
+
+    @BindView(R.id.etx_name_activity_payments)
+    EditText etxFullName;
+
+    @BindView(R.id.etx_amount_activity_payments)
+    EditText etxAmount;
+
+    @BindView(R.id.checkBoxTip)
+    CheckBox checkBoxTip;
+
+    /*
+    * Syntax: @BindView(id_of_view) View objectName;
+    * */
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_payments);
 
-        findViewById(R.id.btn_new_activity)
-                .setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View view)
-                    {
-                        Intent mainActivityIntent = new Intent(
-                                PaymentsActivity.this, ThankYouActivity.class
-                        );
-                        startActivity(mainActivityIntent);
-                    }
-                });
+        /*
+        * Attach ButterKnife to this Activity
+        * */
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_pay_activity_payments)
+    void onPayButtonClick()
+    {
+        /*
+        * This is how you get data from EditText #Janmabhar
+        * */
+        String fullName = etxFullName.getText().toString();
+        String amount = etxAmount.getText().toString();
+
+        /*
+        * Check if Checkbox is Ticked
+        * */
+        boolean toAddTip = checkBoxTip.isChecked();
+
+        /*
+        * Take user to ThankYouActivity
+        * */
+        Intent thankYouIntent = new Intent(PaymentsActivity.this, ThankYouActivity.class);
+        /*
+        * This is how you add extra data to Intent #Janmabhar
+        * */
+        thankYouIntent.putExtra("key_full_name", fullName);
+        thankYouIntent.putExtra("key_amount", amount);
+        thankYouIntent.putExtra("key_to_add_tip", toAddTip);
+        startActivity(thankYouIntent);
+    }
+
+    @OnClick(R.id.txtRateUs)
+    void onRateUsClick()
+    {
+        String link = "https://play.google.com/store/apps/details?id=" + getPackageName();
+        Uri linkInUriFormat = Uri.parse(link);
+
+        /*
+        * Code to throw user to PlayStore
+        * */
+        Intent playStoreIntent = new Intent(Intent.ACTION_VIEW);
+        playStoreIntent.setData(linkInUriFormat);
+        playStoreIntent.setPackage("com.android.vending");
+        startActivity(playStoreIntent);
     }
 }
--- app/src/main/java/com/codeavengers/playingwithintents/ThankYouActivity.java
+++ app/src/main/java/com/codeavengers/playingwithintents/ThankYouActivity.java
@@ -1,16 +1,49 @@
 package com.codeavengers.playingwithintents;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ThankYouActivity extends AppCompatActivity
 {
+    @BindView(android.R.id.content)
+    View currentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+
+        /*
+        * Attach ButterKnife to ThankYouActivity
+        * */
+        ButterKnife.bind(this);
+
+        /*
+        * Catch the Data from Calling or Previous Activity
+        * */
+        Intent fekleliPishvi = getIntent();
+        String fullName = fekleliPishvi.getStringExtra("key_full_name");
+        String amount = fekleliPishvi.getStringExtra("key_amount");
+        boolean toAddTip = fekleliPishvi.getBooleanExtra("key_to_add_tip", false);
+
+        String message = String.format("Thank you %s for Paying Rs. %s. ",fullName, amount);
+        if (toAddTip)
+        {
+            message = message + "Thank you for the Tip.";
+        }
+
+        /*
+        * Write your own UI Logic Here+
+        * */
+        Snackbar.make(currentActivity, message, Snackbar.LENGTH_LONG).show();
     }
 }
